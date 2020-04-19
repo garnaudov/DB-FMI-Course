e11ee948c1ab3d053483612c9b11406b29a83706
Georgi Arnaudov <just_be@abv.bg>
2020-04-19 12:09:13 +0300

    Adding exercises for week 2,3,5,6

---
+++ week2-ManyRelations-exercise.sql
@@ -0,0 +1,119 @@
+﻿
+--I.За базата от данни Movies
+USE movies;
+
+--1. Напишете заявка, която извежда имената на актьорите мъже, участвали във
+--филма The Usual Suspects.
+
+SELECT name FROM moviestar WHERE gender='M'
+INTERSECT
+SELECT starname FROM starsin WHERE movietitle = 'The Usual Suspects';
+
+--2. Напишете заявка, която извежда имената на актьорите, участвали във филми от
+--1995, продуцирани от студио MGM.
+
+SELECT DISTINCT starname
+FROM starsin, movie
+WHERE movieyear=1995 AND studioname='MGM';
+
+--3. Напишете заявка, която извежда имената на продуцентите, които са
+--продуцирали филми на студио MGM.
+
+SELECT DISTINCT name
+FROM movieexec, movie
+WHERE PRODUCERC# = CERT# AND STUDIONAME='MGM';
+
+--4. Напишете заявка, която извежда имената на всички филми с дължина, поголяма от дължината на филма Star Wars.
+
+SELECT title
+FROM movie
+WHERE LENGTH > (
+	SELECT length
+	FROM movie
+	WHERE title='Star Wars'
+);
+
+--5. Напишете заявка, която извежда имената на продуцентите с нетни активи поголеми от тези на Stephen Spielberg.
+
+SELECT name
+FROM movieexec
+WHERE networth > (
+	SELECT networth
+	FROM MOVIEEXEC
+	WHERE name='Stephen Spielberg'
+);
+
+--6. Напишете заявка, която извежда имената на всички филми, които са
+--продуцирани от продуценти с нетни активи по-големи от тези на Stephen
+--Spielberg.
+
+SELECT title
+FROM movie
+WHERE PRODUCERC# in (
+	SELECT cert#
+	FROM movieexec
+	WHERE NETWORTH > (
+		SELECT networth
+		FROM movieexec
+		WHERE name='Stephen Spielberg'
+	)
+)
+
+--II.За базата от данни PC
+USE pc;
+
+--1. Напишете заявка, която извежда производителя и честотата на лаптопите с
+--размер на диска поне 9 GB.
+
+SELECT DISTINCT maker,speed
+FROM laptop l
+JOIN product p ON p.model = l.model 
+WHERE type='Laptop' AND hd>9
+
+--2. Напишете заявка, която извежда модел и цена на продуктите, произведени от
+--производител с име B.
+
+
+--3. Напишете заявка, която извежда производителите, които произвеждат лаптопи,
+--но не произвеждат персонални компютри.
+
+SELECT DISTINCT maker FROM product WHERE type='Laptop'
+EXCEPT
+SELECT DISTINCT maker FROM product WHERE type='PC'
+
+--4. Напишете заявка, която извежда размерите на тези дискове, които се предлагат
+--в поне два различни персонални компютъра (два компютъра с различен код).
+
+--5. Напишете заявка, която извежда двойките модели на персонални компютри,
+--които имат еднаква честота и памет. Двойките трябва да се показват само по
+--веднъж, например само (i, j), но не и (j, i).
+
+USE pc;
+SELECT p1.model as model_1, p2.model as model_2, p1.speed, p1.ram
+FROM pc p1
+JOIN pc p2 ON p1.speed = p2.speed AND p1.ram = p2.ram AND p1.model < p2.model;
+
+--6. Напишете заявка, която извежда производителите на поне два различни
+--персонални компютъра с честота поне 400.
+
+--III.За базата от данни SHIPS
+USE ships;
+
+--1. Напишете заявка, която извежда името на корабите с водоизместимост над
+--50000.
+--2. Напишете заявка, която извежда имената, водоизместимостта и броя оръдия на
+--всички кораби, участвали в битката при Guadalcanal.
+--3. Напишете заявка, която извежда имената на тези държави, които имат както
+--бойни кораби, така и бойни крайцери.
+--4. Напишете заявка, която извежда имената на тези кораби, които са били
+--повредени в една битка, но по-късно са участвали в друга битка.
+
+SELECT DISTINCT * FROM (
+	SELECT o.ship, o.result, b.date
+	FROM outcomes o
+	JOIN battles b ON o.battle=b.name
+	WHERE o.result='damaged') data1	JOIN (
+		SELECT o.ship,o.result,b.date
+		FROM outcomes o
+		JOIN battles b ON o.battle=b.name
+	) data2	ON data1.date < data2.date AND data1.ship = data2.ship
---
+++ week3-Subqueries-exercise.sql
@@ -0,0 +1,212 @@
+﻿--I.За базата от данни Movies
+USE movies;
+
+SELECT CONCAT(title, year) FROM movie;
+
+--1. Напишете заявка, която извежда имената на актрисите, които са също и
+--продуценти с нетни активи над 10 милиона.
+
+--with using subqueries, IN (better)
+SELECT mstar.name
+FROM moviestar mstar
+WHERE gender = 'F' AND name IN (
+	SELECT name
+	FROM movieexec mexec
+	WHERE networth > 10000000
+);
+
+--or with using INTERSECT
+
+SELECT name FROM moviestar WHERE gender='F'
+INTERSECT
+SELECT name	FROM movieexec WHERE networth > 10000000
+
+--2. Напишете заявка, която извежда имената на тези актьори (мъже и жени),
+--които не са продуценти.
+
+SELECT name
+FROM moviestar
+WHERE name NOT IN (
+	SELECT name
+	FROM movieexec
+)
+
+--or 
+
+SELECT name FROM moviestar
+EXCEPT
+SELECT name	FROM movieexec
+
+--3. Напишете заявка, която извежда имената на всички филми с дължина,
+--по-голяма от дължината на филма ‘Star Wars’
+
+SELECT title
+FROM movie
+WHERE length > (
+	SELECT length
+	FROM movie
+	WHERE title='Star Wars'
+);
+
+--4. Напишете заявка, която извежда имената на продуцентите и имената на
+--филмите за всички филми, които са продуцирани от продуценти с нетни
+--активи по-големи от тези на ‘Merv Griffin’
+
+SELECT e.name, m.title
+FROM movie m
+JOIN movieexec e ON m.PRODUCERC# = e.CERT#
+WHERE networth > (
+	SELECT networth
+	FROM movieexec
+	WHERE name='Merv Griffin'
+)
+ORDER BY name, title
+
+--II.За базата от данни PC
+
+USE pc;
+
+--1. Напишете заявка, която извежда производителите на персонални
+--компютри с честота над 500.
+
+SELECT pr.maker
+FROM product pr
+WHERE pr.model IN (
+	SELECT pc.model
+	FROM pc
+	WHERE speed > 500
+)
+
+--2. Напишете заявка, която извежда код, модел и цена на принтерите с най-висока цена.
+
+SELECT code, model, price
+FROM printer
+WHERE price = (
+	SELECT MAX(price)
+	FROM printer
+)
+
+--3. Напишете заявка, която извежда лаптопите, чиято честота е по-ниска от
+--честотата на всички персонални компютри.
+
+SELECT *
+FROM laptop l
+WHERE l.speed < ALL (
+	SELECT pc.speed
+	FROM pc
+)
+--4. Напишете заявка, която извежда модела и цената на продукта (PC,
+--лаптоп или принтер) с най-висока цена.
+
+SELECT all_product_1.model, all_product_1.price
+FROM (
+	SELECT model,price
+	FROM printer
+	UNION ALL
+	SELECT model,price
+	FROM pc
+	UNION ALL
+	SELECT model,price
+	FROM laptop
+) all_product_1
+WHERE price=(
+	SELECT MAX(all_products_2.price)
+	FROM (
+		SELECT model,price
+		FROM printer
+		UNION ALL
+		SELECT model,price
+		FROM pc
+		UNION ALL
+		SELECT model,price
+		FROM laptop
+	) all_products_2
+)
+
+--5. Напишете заявка, която извежда производителя на цветния принтер с
+--най-ниска цена.
+
+SELECT prod.maker
+FROM product prod
+WHERE prod.model IN (
+	SELECT pr.model
+	FROM printer pr
+	WHERE pr.price = (
+		SELECT MIN(price)
+		FROM printer pr
+		WHERE pr.color='y'
+		) AND pr.color='y'
+)
+
+--6. Напишете заявка, която извежда производителите на тези персонални
+--компютри с най-малко RAM памет, които имат най-бързи процесори.
+
+SELECT maker
+FROM product
+WHERE product.model IN (
+	SELECT pc.model
+	FROM pc
+	WHERE ram =(
+		SELECT MIN(ram)	FROM pc
+		) AND speed = (
+			SELECT MAX(pc_min_ram.speed)
+			FROM (
+				SELECT speed
+				FROM pc
+				WHERE ram =(
+					SELECT MIN(ram)
+					FROM pc
+					)
+			) pc_min_ram
+		) 
+)
+
+--III.За базата от данни SHIPS
+USE ships;
+
+--1. Напишете заявка, която извежда страните, чиито кораби са с най-голям
+--брой оръдия.
+
+SELECT DISTINCT c.country
+FROM classes c
+WHERE c.numguns = (
+	SELECT MAX(c.numguns)
+	FROM classes c
+)
+
+--2. Напишете заявка, която извежда класовете, за които поне един от
+--корабите е потънал в битка.
+
+SELECT DISTINCT s.class
+FROM ships s
+WHERE s.name IN (
+	SELECT o.ship
+	FROM outcomes o
+	WHERE o.result = 'sunk'
+)
+
+--3. Напишете заявка, която извежда името и класа на корабите с 16 инчови
+--оръдия.
+
+SELECT s.name, s.class
+FROM ships s
+WHERE s.class IN (
+	SELECT c.class
+	FROM classes c
+	WHERE c.bore='16'
+)
+
+--4. Напишете заявка, която извежда имената на битките, в които са
+--участвали кораби от клас ‘Kongo’.
+
+SELECT o.battle
+FROM outcomes o
+WHERE o.ship IN (
+	SELECT s.name
+	FROM ships s
+	WHERE s.class = 'Kongo'
+)
+
+--5. Напишете заявка, която извежда класа и името на корабите, чиито брой
+--оръдия е по-голям или равен на този на корабите със същия калибър
+--оръдия.
